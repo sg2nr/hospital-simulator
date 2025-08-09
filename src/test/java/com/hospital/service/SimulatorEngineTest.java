69a6197002a0a254fe92e5b437d3c9f8f87446af
sg2nr <20168606+sg2nr@users.noreply.github.com>
2025-08-09 18:05:42 +0200

    Add test for simulation with multiple health states and drugs in SimulatorEngineTest

--- src/test/java/com/hospital/service/SimulatorEngineTest.java
+++ src/test/java/com/hospital/service/SimulatorEngineTest.java
@@ -86,4 +86,27 @@ class SimulatorEngineTest {
     assertEquals(0, response.patientsByState().get(HealthState.TUBERCULOSIS));
     assertEquals(0, response.patientsByState().get(HealthState.DEAD));
   }
+
+  @Test
+  void testSimulateWhenAllRulesAreAppliedToHealthyAndTuberculosisAndFeverAndDiabetesAndAntibioticAndInsulinShouldReturnThreeFeverAndOneDiabetes() {
+    // Given
+    SimulationRequest request = new SimulationRequest(
+            Map.of(HealthState.TUBERCULOSIS, 1, HealthState.FEVER, 1, HealthState.DIABETES, 1, HealthState.HEALTHY, 1),
+            Set.of(Drug.ANTIBIOTIC, Drug.INSULIN));
+
+    List<Rule> rules = List.of(new AspirinRule(), new AntibioticRule(), new InsulinRule(), new ParacetamolRule(),
+            new FlyingSpaghettiMonsterRule(new Random()));
+    SimulatorEngine simulatorEngine = new SimulatorEngine(rules);
+
+    // When
+    SimulationResponse response = simulatorEngine.simulate(request);
+
+    // Then
+    assertEquals(3, response.patientsByState().get(HealthState.FEVER));
+    assertEquals(0, response.patientsByState().get(HealthState.HEALTHY));
+    assertEquals(1, response.patientsByState().get(HealthState.DIABETES));
+    assertEquals(0, response.patientsByState().get(HealthState.TUBERCULOSIS));
+    assertEquals(0, response.patientsByState().get(HealthState.DEAD));
+  }
+
 }
